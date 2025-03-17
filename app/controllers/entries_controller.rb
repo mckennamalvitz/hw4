beabee0994662b6686fcd89f28de5835ecb35a8b
McKenna Malvitz <mckenna.malvitz@kellogg.northwestern.edu>
2025-03-17 05:48:14 +0000

    added photos

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -15,6 +15,7 @@ class EntriesController < ApplicationController
       @entry["description"] = params["description"]
       @entry["occurred_on"] = params["occurred_on"]
       @entry["place_id"] = params["place_id"]
+      @entry.uploaded_image.attach(params["uploaded_image"])
       @entry["user_id"] = session["user_id"] 
       @entry.save
     end 
