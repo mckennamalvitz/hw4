56ac2db1135f85724a624a2f6d3f3032575a8094
McKenna Malvitz <mckenna.malvitz@kellogg.northwestern.edu>
2025-03-17 06:19:32 +0000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     if User.find_by({ "email" => params["email"] }) == nil
       @user = User.new
-      @user["first_name"] = params["first_name"]
+      #@user["first_name"] = params["first_name"]
       #@user["last_name"] = params["last_name"]
       @user["username"] = params["username"]
       @user["email"] = params["email"]
